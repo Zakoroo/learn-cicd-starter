41ac2a46151539255da07463317f9ae538b3e3fb
Zakoroo <maithemhussein27@gmail.com>
2026-06-26 01:46:46 +0200

    test(auth): added test for the get ApiKey function

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,30 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	// Check if the header does not exist
+	header := make(http.Header)
+	_, err := GetAPIKey(header)
+	if err == nil {
+		t.Fatalf("expected: %v, got: %v", ErrNoAuthHeaderIncluded, err)
+	}
+
+	// Check if the header is correctly formatted
+	header.Set("APIAuthorization", "nonesense")
+	_, err = GetAPIKey(header)
+	if err == nil {
+		t.Fatalf("expected: %v, got: %v", errors.New("malformed authorization header"), err)
+	}
+
+	// Check if the return value is correct for the correct headers
+	header.Set("Authorization", "ApiKey 1234")
+	res, err := GetAPIKey(header)
+	if res != "12345" || err != nil {
+		t.Fatalf("expected: (%v, %v), got: (%v, %v)", "1234", nil, res, err)
+	}
+}
